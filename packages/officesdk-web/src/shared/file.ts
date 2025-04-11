d0501663bd6dedcde643455d4b961467cc660407
chenyang <chenyang@shimo.im>
2025-04-11 16:13:47 +0800

    Add file type

---
+++ packages/officesdk-web/src/shared/file.ts
@@ -0,0 +1,11 @@
+/**
+ * Supported file types for the Office SDK.
+ */
+export enum FileType {
+  Document = 'document', // Document file, like .doc, .docx
+  Spreadsheet = 'spreadsheet', // Spreadsheet file, like .xls, .xlsx
+  Presentation = 'presentation', // Presentation file, like .ppt, .pptx
+  LiteDoc = 'ltdoc', // Lite document.
+  DBTable = 'dbtable', // Database table.
+  Pdf = 'pdf', // PDF file.
+}
